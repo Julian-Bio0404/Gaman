3da783d43e95c663848ce1a6a0959f9d4a3d1d94
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2021-11-10 17:59:00 -0500

    Club invitation model

--- gaman/sports/models/__init__.py
+++ gaman/sports/models/__init__.py
@@ -1,3 +1,4 @@
 from .clubs import *
+from .invitations import *
 from .leagues import *
 from .members import *
---
+++ gaman/sports/models/invitations.py
@@ -0,0 +1,27 @@
+"""Invitation models."""
+
+# Django
+from django.db import models
+
+# Utils
+from gaman.utils.models import GamanModel
+
+
+class Invitation(GamanModel):
+    """Invitation model."""
+
+    issued_by = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE,
+        help_text='Club coach that is providing the invitation')
+    
+    invited = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE,
+        help_text='Athlete that used the invitation to enter the club.',
+        related_name='invited')
+    
+    club = models.ForeignKey('sports.Club', on_delete=models.CASCADE)
+    used = models.BooleanField(default=False)
+
+    def __str__(self):
+        """Return club and athlete."""
+        return f'{self.club}: {self.athlete}'
