6a2789c1a79633641cf1a185a76a45a9f9d36f52
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-01-22 00:51:28 -0500

    Updated libraries

--- config/urls.py
+++ config/urls.py
@@ -9,10 +9,10 @@ from django.urls import include, path
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path(r'auth/', include('rest_framework_social_oauth2.urls')),
     path('', include(('gaman.users.urls', 'users'), namespace='users')),
     path('', include(('gaman.posts.urls', 'posts'), namespace='posts')),
     path('', include(('gaman.sponsorships.urls', 'sponsorships'), namespace='sponsorships')),
     path('', include(('gaman.sports.urls', 'sports'), namespace='sports')),
+    # path(r'auth/', include('rest_framework_social_oauth2.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
