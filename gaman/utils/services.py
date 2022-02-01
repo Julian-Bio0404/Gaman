041d62813be54972ab6cdeb635af499a75b5d6c6
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-01-31 23:25:35 -0500

    Changes in api maps service

--- gaman/utils/services.py
+++ gaman/utils/services.py
@@ -14,19 +14,18 @@ def get_ubication(place: str)-> dict:
     """
     params = {'q': place, 'apiKey': settings.API_MAPS_KEY}
     response = requests.get(settings.API_MAPS_URL, params=params)
+    response = response.json()
 
-    place = response.json()['items'][0]['title']
-    country = response.json()['items'][0]['address']['countryName']
-    state = response.json()['items'][0]['address']['county']
-    city = response.json()['items'][0]['address']['city']
-    lat = response.json()['items'][0]['position']['lat']
-    lng = response.json()['items'][0]['position']['lng']
-    geolocation = f'{lat} {lng}'
+    place = response['items'][0]['title']
+    country = response['items'][0]['address']['countryName']
+    state = response['items'][0]['address']['county']
+    city = response['items'][0]['address']['city']
+    lat = response['items'][0]['position']['lat']
+    lng = response['items'][0]['position']['lng']
 
     ubication = {
         'country': country, 'state': state,
         'city': city, 'place': place,
-        'geolocation': geolocation
+        'geolocation': f'{lat} {lng}'
     }
-
     return ubication
