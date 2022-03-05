2a907b240c9a2192884dc2ebe93045f11cfd1d36
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-03-04 19:01:35 -0500

    Enviroment conditionals

--- manage.py
+++ manage.py
@@ -7,7 +7,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    if 'test' in sys.argv:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.test')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
