4e65c9e45d96736e4a056539d4829ed9a8635de6
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-04-30 22:18:58 -0500

    Update tasks and signals

--- gaman/sports/signals.py
+++ gaman/sports/signals.py
@@ -1,35 +1,16 @@
 """Sports signals."""
 
 # Django
-from django.db.models.signals import post_save, pre_delete, pre_save
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
 # Models
 from gaman.sports.models import SportEvent
 
 # Tasks
-from taskapp.tasks.events import create_sport_event, delete_sport_event
-
-
-@receiver(post_save, sender=SportEvent)
-def post_save_event(sender, instance, created, *args, **kwargs):
-    if created:
-        data = {
-            'title': instance.title,
-            'description': instance.description,
-            'start': instance.start,
-            'finish': instance.finish,
-            'geolocation': instance.geolocation,
-            'country': instance.country,
-            'state': instance.state,
-            'city': instance.city,
-            'place': instance.place,
-            'created': instance.created,
-            'updated': instance.updated
-        }
-        create_sport_event.delay(data)
+from taskapp.tasks.events import delete_sport_event
 
 
 @receiver(pre_delete, sender=SportEvent)
 def post_delete_event(sender, instance, *args, **kwargs):
-    delete_sport_event.delay(instance.id)
+    delete_sport_event.delay(instance)
--- taskapp/tasks/events.py
+++ taskapp/tasks/events.py
@@ -11,35 +11,20 @@ from django.conf import settings
 # Celery
 from taskapp.celery import app
 
+# Models
+from gaman.sports.models import SportEvent
+
 
 @app.task(bind=True)
-def create_sport_event(self, data: dict):
-    """Send sport event to geogaman service."""
-    url = settings.GEOGAMAN_DOMAIN + 'events/'
+def delete_sport_event(self, instance: SportEvent):
+    """Delete sport event in geogaman service."""
+    url = settings.GEOGAMAN_DOMAIN + 'zones/delete_events/'
+    data = {
+        'event_id': instance.pk,
+        'geolocation': instance.geolocation
+    }
     try:
         response = requests.post(url, data=data)
-        return 'Success' if response.status_code == 201 else 'Unsuccess'
-    except:
-        return 'Unsuccess'
-
-
-@app.task(bind=True)
-def update_sport_event(self, data: dict, event_id: int):
-    """Send update of sport event to geogaman service."""
-    url = settings.GEOGAMAN_DOMAIN + 'events/' + str(event_id)
-    try:
-        response = requests.patch(url, data=data)
         return 'Success' if response.status_code == 200 else 'Unsuccess'
-    except:
-        return 'Unsuccess'
-
-
-@app.task(bind=True)
-def delete_sport_event(self, event_id: int):
-    """Send delete sport event to geogaman service."""
-    url = settings.GEOGAMAN_DOMAIN + 'events/' + str(event_id)
-    try:
-        response = requests.delete(url)
-        return 'Success' if response.status_code == 204 else 'Unsuccess'
     except:
         return 'Unsuccess'
